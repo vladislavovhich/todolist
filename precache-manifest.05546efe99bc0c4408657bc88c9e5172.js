5a97b3d734f2de2e5d40d3719cd9fbcfad1049b6
VladVorobiov <vladislavovhich@gmail.com>
2020-08-18 21:54:43 +0300

    Updates

---
+++ precache-manifest.05546efe99bc0c4408657bc88c9e5172.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5620e3566625923444fb7707e7799f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "69fae6b5722e130b7360",
+    "url": "/todolist/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7936a55015bb855f165a",
+    "url": "/todolist/static/css/main.a29dc8ea.chunk.css"
+  },
+  {
+    "revision": "69fae6b5722e130b7360",
+    "url": "/todolist/static/js/2.5d85243e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.5d85243e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7936a55015bb855f165a",
+    "url": "/todolist/static/js/main.811117ed.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
