69c7b522345a97cebce8e7ac870bf46771aae2a4
VladVorobiov <vladislavovhich@gmail.com>
2020-08-18 22:01:31 +0300

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,17 @@ import {Provider} from "react-redux";
 import store from "./store/store"
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {faTimes, faCheckSquare, faClipboardList, faCog, faTrashAlt, faTimesCircle, faPlus} from "@fortawesome/free-solid-svg-icons";
+import {HashRouter} from "react-router-dom";
 
 library.add(faTimes, faCheckSquare, faClipboardList, faCog, faTrashAlt, faTimesCircle, faPlus)
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <HashRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
